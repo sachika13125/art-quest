18666eab5a64a7f909b84a3de95e6708c117d4a3
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2023-10-09 15:41:53 -0700

    add js to index.html and myQuiz.html

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// Navigation bar
-function toggleMobileMenu() {
-    let mobileMenu = document.getElementById('mobile-menu');
-    if (mobileMenu.classList.contains('hidden')) {
-        mobileMenu.classList.remove('hidden');
-    } else {
-        mobileMenu.classList.add('hidden');
-    }
-}
-
-
-
 
 //Quiz
 
