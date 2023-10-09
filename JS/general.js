a55b7fc243f864a4a2a0cf0c1d232caae70508b7
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2023-10-09 15:40:54 -0700

    touch general.js

---
+++ JS/general.js
@@ -0,0 +1,9 @@
+// Navigation bar
+function toggleMobileMenu() {
+    let mobileMenu = document.getElementById('mobile-menu');
+    if (mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.remove('hidden');
+    } else {
+        mobileMenu.classList.add('hidden');
+    }
+}
